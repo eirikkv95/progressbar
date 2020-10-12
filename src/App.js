d75f2e89bb3ed29bc4cdb545d05acc969aad66cb
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-10-12 03:00:15 +0200

    basic setup. More styles and Folder structure to follow

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Progress from './components/Progress/Progress';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Progress />
+    </div>
+  );
+}
---
+++ src/components/Progress/Progress.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Progress = () => {
+  return (
+    <>
+      <h1>Here is the state</h1>
+    </>
+  );
+};
+
+export default Progress;
