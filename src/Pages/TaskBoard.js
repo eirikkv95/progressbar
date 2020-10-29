efb63d7ce58a5e4a58b1818178802089aa46a221
Eirik Kalvø Vattøy <eirikkalven@gmail.com>
2020-10-29 16:41:10 +0100

    Space from the top of the page

--- src/Pages/TaskBoard.js
+++ src/Pages/TaskBoard.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import TaskBoardConteiner from '../components/TaskBoard/taskBoardContainer';
 
 const TaskBoard = () => {
   return (
-    <>
+    <Space>
       <TaskBoardConteiner />
-    </>
+    </Space>
   );
 };
 
 export default TaskBoard;
+
+const Space = styled.div`
+  padding-top: 15rem;
+`;
